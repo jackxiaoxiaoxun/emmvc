f58dd4a79db285ca6207c9cd3b983b3aa908105e
jack.xun <jkqi@sohu.com>
2016-03-08 21:26:34 +0800

    some change

--- Emmvc/Model.php
+++ Emmvc/Model.php
@@ -9,14 +9,16 @@ namespace Emmvc;
 
 class Model
 {
-	
+	/**
+	 * mysql query builder
+	 * @var Db
+	 */
 	public $db;
 	
 	public function __construct()
 	{
-		$this->db	= '';
+		$this->db	= Service::Db();
 	}
-	
 	
 	
 	
